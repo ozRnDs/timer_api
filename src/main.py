612cb67f7a0fb5f3f4dc1bdc0c41d399e698c396
oz <oz.rnds@gmail.com>
2023-09-20 01:59:21 +0300

    feat(main): Connect uvicorn to 0.0.0.0. Add Documentations

--- src/main.py
+++ src/main.py
@@ -3,11 +3,14 @@ from config.config import app_config
 from fastapi import FastAPI
 import uvicorn
 
-app = FastAPI()
+app = FastAPI(description="Rest API Interface for the timer service")
 
 from routes.timer import timers_router
 app.include_router(timers_router, prefix="/timer")
 
+from routes.auto_response import auto_router
+app.include_router(auto_router)
+
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=5000, log_level="info", access_log=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info", access_log=False)
